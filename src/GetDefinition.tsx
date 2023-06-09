8c92af1bbc0b89cbdd6cabda764074bdf08bdd32
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-06-09 19:35:22 +0000

    added error output for when dictionary api returns 404, so game won't crash

--- src/GetDefinition.tsx
+++ src/GetDefinition.tsx
@@ -7,22 +7,31 @@ type GetDefinitionProps = {
 
 const GetDefinition: React.FC<GetDefinitionProps> = ({ word }) => {
   const [definitionData, setDefinitionData] = useState<any | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
       .then(response => response.json())
       .then(data => {
-        if (data[0]) {
-          setDefinitionData(data[0]);
+        if (data.title && data.title === "No Definitions Found") {
+          setErrorMessage('No definition found');
         } else {
-          setDefinitionData('No definition found');
+          setDefinitionData(data[0]);
         }
       })
       .catch(error => {
         console.error('Error:', error);
-        setDefinitionData('Error fetching definition');
+        setErrorMessage('Error fetching definition');
       });
   }, [word]);
+
+  if (errorMessage) {
+    return (
+      <Box color="gray.500" fontSize="sm" textAlign="left" mt="0.5">
+        <Text>{errorMessage}</Text>
+      </Box>
+    );
+  }
 
   return (
     <Box color="gray.500" fontSize="sm" textAlign="left" mt="0.5">
@@ -49,7 +58,7 @@ const GetDefinition: React.FC<GetDefinitionProps> = ({ word }) => {
           })}
         </>
       ) : (
-        <Text>Error fetching definition</Text>
+        <Text>Loading...</Text>
       )}
     </Box>
   );
