c1c240ca8bbe90e82b5cd1af3f401182b5edf160
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-06-13 18:58:16 +0000

    fixed indentations, and improved redundant styling code.

--- src/LearnModal.tsx
+++ src/LearnModal.tsx
@@ -16,59 +16,78 @@ type LearnModalProps = {
   onClose: () => void;
 };
 
+const generateText = (
+  text: string,
+  color: string,
+  fontWeight: string
+): JSX.Element => (
+  <Text as="span" color={color} fontWeight={fontWeight}>
+    {text}
+  </Text>
+);
+
 const LearnModal: React.FC<LearnModalProps> = ({ isOpen, onClose }) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}  size="md">
+    <Modal isOpen={isOpen} onClose={onClose} size="md">
       <ModalOverlay />
       <ModalContent bg="gray.100">
-        <ModalHeader textAlign="center">How To Play <Text as="span" color="green.500" fontWeight="bold">WORD</Text>→<Text as="span" color="blue.500" fontWeight="bold">PATH</Text>  </ModalHeader>
+        <ModalHeader textAlign="center">
+          How To Play{" "}
+          {generateText("WORD", "green.500", "bold")} →
+          {generateText("PATH", "blue.500", "bold")}
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          <div>
             <Text mb={4}>
-    Get from <Text as="span" color="green.500" fontWeight="bold">START</Text> to <Text as="span" color="blue.500" fontWeight="bold">GOAL</Text> in as few words as possible.
-  </Text>
+              Get from{" "}
+              {generateText("START", "green.500", "bold")} to{" "}
+              {generateText("GOAL", "blue.500", "bold")} in as few words as possible.
+            </Text>
 
-          <Text mb="4">
-            You  can <Text as="span" color="green" fontWeight="bold">add</Text>, <Text as="span" color="red" fontWeight="bold">remove</Text>, <Text as="span" color="orange.500" fontWeight="bold">change</Text>, or <Text as="span" color="purple" fontWeight="bold">swap</Text> one letter at a time to make new words.
-          </Text>
+            <Text mb="4">
+              You can{" "}
+              {generateText("add", "green", "bold")},{" "}
+              {generateText("remove", "red", "bold")},{" "}
+              {generateText("change", "orange.500", "bold")}, or{" "}
+              {generateText("swap", "purple", "bold")} one letter at a time to make new words.
+            </Text>
 
-          <Text mb="2">
-            Like this:
-          </Text>
-          <Text mb="2">
-            <Text as="span" color="green"fontWeight="bold">add</Text>: 
+            <Text mb="2">Like this:</Text>
+            <Text mb="2">
+              {generateText("add", "green", "bold")}: <br />
+              at → {generateText("r", "green", "bold")}at → rat
+              {generateText("s", "green", "bold")} → rat
+              {generateText("e", "green", "bold")}s
+            </Text>
+
+            <Text mb="2">
+              {generateText("change", "orange.500", "bold")}: <br />
+              date → {generateText("m", "orange.500", "bold")}ate → ma
+              {generateText("l", "orange.500", "bold")}e → {generateText("p", "orange.500", "bold")}ale
+            </Text>
+
+            <Text mb="2">
+              {generateText("remove", "red", "bold")}: <br />
+              like this: sta{generateText("r", "red", "bold")}red → stare{generateText("d", "red", "bold")} → star{generateText("e", "red", "bold")} → sta{generateText("r", "red", "bold")} → tar
+            </Text>
+
+            <Text mb="2">
+              {generateText("swap", "purple", "bold")}: <br />
+              {generateText("r", "purple", "bold")}a{generateText("c", "purple", "bold")}e → {generateText("ca", "purple", "bold")}re → acre
+            </Text>
+
             <br />
-           at → <Text as="span" color="green"fontWeight="bold">r</Text>at → rat<Text as="span" color="green"fontWeight="bold">s</Text> → rat<Text as="span" color="green"fontWeight="bold">e</Text>s
-          </Text>
-
-          <Text mb="2">
-            <Text as="span" color="orange.500"fontWeight="bold">change</Text>: 
+            <Text mb="4">
+              Try practice mode to learn how to play.
+              <br />
+              <br />A new, five round game daily!
+            </Text>
             <br />
-            date → <Text as="span" color="orange.500"fontWeight="bold">m</Text>ate → ma<Text as="span" color="orange.500"fontWeight="bold">l</Text>e → <Text as="span" color="orange.500"fontWeight="bold">p</Text>ale
-          </Text>
-
-          <Text mb="2">
-            <Text as="span" color="red"fontWeight="bold">remove</Text>: 
-            <br />
-          like this: sta<Text as="span" color="red"fontWeight="bold">r</Text>red → stare<Text as="span" color="red"fontWeight="bold">d</Text> → star<Text as="span" color="red"fontWeight="bold">e</Text> → sta<Text as="span" color="red"fontWeight="bold">r</Text> → tar
-          </Text>
-
-          <Text mb="2">
-            <Text as="span" color="purple"fontWeight="bold">swap</Text>: 
-            <br />
-              <Text as="span" color="purple"fontWeight="bold">r</Text>a<Text as="span" color="purple"fontWeight="bold">c</Text>e → <Text as="span" color="purple"fontWeight="bold">ca</Text>re → acre
-          </Text>
-
-          <br />
-          <Text mb="4">
-            Try practice mode to learn how to play.
-            <br />
-            <br />A new, five round game daily!
-          </Text>
-          <br />
-          <Button colorScheme='blue' mr={3} onClick={onClose}>
-            OK
-          </Button>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              OK
+            </Button>
+          </div>
         </ModalBody>
 
         <ModalFooter>
@@ -78,13 +97,11 @@ const LearnModal: React.FC<LearnModalProps> = ({ isOpen, onClose }) => {
             bottom="5"
             display="flex"
             justifyContent="center"
-          >
-          </Box>
-
+          ></Box>
         </ModalFooter>
       </ModalContent>
     </Modal>
-  )
-}
+  );
+};
 
 export default LearnModal;
