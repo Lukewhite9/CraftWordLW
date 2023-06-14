1516a9be30bbd771fdd5840716f3128dfdd51be0
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-06-14 22:16:00 +0000

    additional text edits for content and formatting

--- src/AboutModal.tsx
+++ src/AboutModal.tsx
@@ -10,19 +10,31 @@ import {
   Text,
   Box
 } from '@chakra-ui/react';
-
+const generateText = (
+  text: string,
+  color: string,
+  fontWeight: string
+): JSX.Element => (
+  <Text as="span" color={color} fontWeight={fontWeight}>
+    {text}
+  </Text>
+);
 export default function AboutModal({ isOpen, onClose }) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader textAlign="center">About This Game</ModalHeader>
+        <ModalHeader textAlign="center">
+          About{" "}
+          {generateText("WORD", "green.500", "bold")} →
+          {generateText("PATH", "blue.500", "bold")}
+        </ModalHeader>
         <ModalCloseButton />
 <ModalBody>
-  <Text mb={4}>Wordpath is a game about transforming words. Each new word is a step on a path to the goal word. The shorter the path, the better your score. The best path might have words you don't use every day, but only real words in English are allowed. </Text>
-  <Text mb={5}>I thought of Wordpath while watching my four year old son learn to spell. I learned to code the game by asking ChatGPT how to make it, and then I finished it with a human friend. </Text>
-  <Text mb={4}>Thanks for playing!</Text>
-  <Text mb={7}>-Lukematik</Text>
+  <Text mb={4}>Wordpath is a game where you transform words. Each new word is a step on your path to the goal word. The shorter your path, the better your score. The best path might use words you don't hear every day, but only real words in English are allowed. </Text>
+  <Text mb={5}>I thought of Wordpath while watching my four year old son learn to spell. I learned to code the game by asking ChatGPT how to make it, and then I finished it with a human friend. Thanks for playing!</Text>
+  <Text mb={6}></Text>
+  <Text ml={8} mb={4}>-Lukematik</Text>
   <Box display="flex" justifyContent="flex-end">
     <Button variant="outline" onClick={onClose}>
       Close
