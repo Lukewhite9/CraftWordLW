a1a6fea9f04ddaedb87b285cc39a10e0aab23336
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-06-16 20:16:25 +0000

    added the ability to move last letter to front of word, or first letter to back of word

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
 import wordPairs from './wordpairs_5dict.json';
 
 export const isValidTransformation = (word1: string, word2: string) => {
-  let len_diff = word1.length - word2.length;
+  const len_diff = word1.length - word2.length;
 
   if (Math.abs(len_diff) === 1) {
     if (len_diff === 1) {
       // Remove a letter
       for (let i = 0; i < word1.length; i++) {
-        let modified_word = word1.slice(0, i) + word1.slice(i + 1);
+        const modified_word = word1.slice(0, i) + word1.slice(i + 1);
         if (modified_word.toLowerCase() === word2.toLowerCase()) {
           return true;
         }
@@ -16,7 +16,7 @@ export const isValidTransformation = (word1: string, word2: string) => {
       // Add a letter
       for (let i = 0; i <= word1.length; i++) {
         for (let letter of 'abcdefghijklmnopqrstuvwxyz') {
-          let new_word = word1.slice(0, i) + letter + word1.slice(i);
+          const new_word = word1.slice(0, i) + letter + word1.slice(i);
           if (new_word.toLowerCase() === word2.toLowerCase()) {
             return true;
           }
@@ -24,26 +24,37 @@ export const isValidTransformation = (word1: string, word2: string) => {
       }
     }
   } else if (len_diff === 0) {
-    // Swap any two letters
-    for (let i = 0; i < word1.length; i++) {
-      for (let j = i + 1; j < word1.length; j++) {
-        let new_word =
-          word1.slice(0, i) +
-          word1[j] +
-          word1.slice(i + 1, j) +
-          word1[i] +
-          word1.slice(j + 1);
-        if (new_word.toLowerCase() === word2.toLowerCase()) {
-          return true;
-        }
+    // Swap any two adjacent letters
+    for (let i = 0; i < word1.length - 1; i++) {
+      const new_word =
+        word1.slice(0, i) +
+        word1[i + 1] +
+        word1[i] +
+        word1.slice(i + 2);
+      if (new_word.toLowerCase() === word2.toLowerCase()) {
+        return true;
       }
+    }
+
+    // Swap the last letter to the front
+    const new_word_last_to_front =
+      word1.slice(-1) + word1.slice(0, -1);
+    if (new_word_last_to_front.toLowerCase() === word2.toLowerCase()) {
+      return true;
+    }
+
+    // Swap the first letter to the end
+    const new_word_first_to_end =
+      word1.slice(1) + word1.slice(0, 1);
+    if (new_word_first_to_end.toLowerCase() === word2.toLowerCase()) {
+      return true;
     }
 
     // Change a single letter
     for (let i = 0; i < word1.length; i++) {
       for (let letter of 'abcdefghijklmnopqrstuvwxyz') {
         if (word1[i].toLowerCase() !== letter.toLowerCase()) {
-          let new_word = word1.slice(0, i) + letter + word1.slice(i + 1);
+          const new_word = word1.slice(0, i) + letter + word1.slice(i + 1);
           if (new_word.toLowerCase() === word2.toLowerCase()) {
             return true;
           }
@@ -54,6 +65,7 @@ export const isValidTransformation = (word1: string, word2: string) => {
 
   return false;
 }
+
 
 
 export const isValidWord = (word: string, wordList: string[]) => {
