cb10d444988d68799f5a5be487e9983aaf69001f
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-07-21 17:13:45 +0000

    undid cheat mode

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,48 +1,6 @@
 export const isValidTransformation = (word1: string, word2: string) => {
-  const len_diff = word1.length - word2.length;
-
-  if (Math.abs(len_diff) === 1) {
-    if (len_diff === 1) {
-      // Remove a letter
-      for (let i = 0; i < word1.length; i++) {
-        const modified_word = word1.slice(0, i) + word1.slice(i + 1);
-        if (modified_word.toLowerCase() === word2.toLowerCase()) {
-          return true;
-        }
-      }
-    } else {
-      // Add a letter
-      for (let i = 0; i <= word1.length; i++) {
-        for (let letter of 'abcdefghijklmnopqrstuvwxyz') {
-          const new_word = word1.slice(0, i) + letter + word1.slice(i);
-          if (new_word.toLowerCase() === word2.toLowerCase()) {
-            return true;
-          }
-        }
-      }
-    }
-  } else if (len_diff === 0) {
-    // Check for anagrams
-    const sorted_word1 = word1.toLowerCase().split('').sort().join('');
-    const sorted_word2 = word2.toLowerCase().split('').sort().join('');
-    if (sorted_word1 === sorted_word2) {
-      return true;
-    }
-
-    // Change a single letter
-    for (let i = 0; i < word1.length; i++) {
-      for (let letter of 'abcdefghijklmnopqrstuvwxyz') {
-        if (word1[i].toLowerCase() !== letter.toLowerCase()) {
-          const new_word = word1.slice(0, i) + letter + word1.slice(i + 1);
-          if (new_word.toLowerCase() === word2.toLowerCase()) {
-            return true;
-          }
-        }
-      }
-    }
-  }
-
-  return false;
+  
+  return true;
 }
 
 export const isValidWord = (word: string, wordList: string[]) => {
