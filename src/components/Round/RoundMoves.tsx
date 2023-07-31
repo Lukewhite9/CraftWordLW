5cce4cbcc7164c993c52057898163a70e62a51d1
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-07-31 06:58:34 -0400

    Revert "Winning move animation"

--- src/components/Round/RoundMoves.tsx
+++ src/components/Round/RoundMoves.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Tag } from "@chakra-ui/react";
 import ReactTextTransition, { presets } from "react-text-transition";
-import MovingText from 'react-moving-text';
 
 type RoundMovesProps = {
   moves: string[];
@@ -56,8 +55,6 @@ const MoveTag: React.FC<{ word: string; colorScheme?: any }> = ({ word, colorSch
 
 const RoundMoves: React.FC<RoundMovesProps> = ({ moves, start, goal, maxMoves }) => {
   const [newWord, setNewWord] = useState("");
-  const [isWinningMove, setIsWinningMove] = useState(false);
-  const [startSecondAnimation, setStartSecondAnimation] = useState(false);
 
   const remainingList = moves.length > 1 ? moves.slice(0, -1) : [];
 
@@ -67,28 +64,15 @@ const RoundMoves: React.FC<RoundMovesProps> = ({ moves, start, goal, maxMoves })
       const currentWord = moves.slice(-1)[0];
       const priorWord = moves.length > 1 ? moves.slice(-2)[0] : start;
       setNewWord(priorWord);
-      setStartSecondAnimation(false);
 
       timer = setTimeout(() => {
         setNewWord(currentWord);
-        if (currentWord === goal) {
-          setIsWinningMove(true);
-        }
       }, 500);
-
-      const timer2 = setTimeout(() => {
-        setStartSecondAnimation(true);
-      }, 1000);
-
-      return () => { clearTimeout(timer); clearTimeout(timer2); }
     } else {
       setNewWord("");
     }
-  }, [moves.length, start, goal]);
-
-  const onAnimationEnd = () => {
-    setIsWinningMove(false);
-  };
+    return () => { timer && clearTimeout(timer); }
+  }, [moves.length, start]);
 
   return (
     <Box display="flex" flexWrap="wrap"  w="100%" m={-4}>
@@ -97,38 +81,7 @@ const RoundMoves: React.FC<RoundMovesProps> = ({ moves, start, goal, maxMoves })
         <MoveTag key={`${word}-${i}`} word={word} />
       ))}
       {newWord.length > 0 && (
-        <MoveTag>
-          <>
-            {newWord.split("").map((letter, index) => (
-              <React.Fragment key={index}>
-                {isWinningMove && startSecondAnimation ? (
-                  <MovingText
-                    onAnimationEnd={index === newWord.length - 1 ? onAnimationEnd : undefined}
-                    type="bounce"
-                    duration="1000ms"
-                    delay={`${index * 20}ms`}
-                    direction="alternate"
-                    timing="ease"
-                    iteration="1"
-                    fillMode="none"
-                  >
-                    {letter}
-                  </MovingText>
-                ) : (
-                  <ReactTextTransition
-                    springConfig={presets.stiff}
-                    translateValue="45%"
-                    inline
-                  >
-                    {letter}
-                  </ReactTextTransition>
-                )}
-              </React.Fragment>
-            ))}
-          </>
-        </MoveTag>
         <MoveTag word={newWord} />
-
       )}
 
       {maxMoves !== Infinity ? 
